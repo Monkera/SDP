b56ca1b0524258b0645ce3bb85ed56ce7cdaa929
jann <jannandrea.f@gmail.com>
2023-02-08 10:46:39 +0100

    volume options added

--- Assets/SettingsMenu.cs
+++ Assets/SettingsMenu.cs
@@ -1,7 +1,16 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class SettingsMenu : MonoBehaviour {
+
+    public AudioMixer MainMixer;
+
+    public void SetVolume(float volume){
+
+        MainMixer.SetFloat("volume", volume);
+
+    }
 
     public void SetFullscreen(bool isFullscreen){
         Screen.fullScreen = isFullscreen;
