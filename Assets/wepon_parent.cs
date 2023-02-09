7ccda6822a1295bdcc5b9ea78478cce8f1a7f0d2
Potestas06 <Patrick@krispel.com>
2023-02-09 10:59:13 +0100

    added an attack

--- Assets/wepon_parent.cs
+++ Assets/wepon_parent.cs
@@ -1,16 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class wepon_parent : MonoBehaviour
 {
+    private Collider2D _collider2D;
     public Vector2 pointer_position { get; set; }
+    private Animator _animator;
+    private GameObject child;
+
+    private void Start()
+    {
+        child = transform.GetChild(0).gameObject;
+        _collider2D = child.GetComponent<Collider2D>();
+        _animator = child.GetComponent<Animator>();
+    }
+    
 
     private void Update()
     {
         pointer_position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.right = (pointer_position - (Vector2)transform.position).normalized;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            _animator.SetBool("attack", true);
+            _collider2D.enabled = true;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            _animator.SetBool("attack", false);
+            _collider2D.enabled = false;
+        }
     }
+
+    
 
 
 }
